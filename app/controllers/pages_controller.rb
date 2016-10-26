2091bba5aba2872cab194c956e8cc6b67c410fa8
Jacob Varghese <jacobv1992@gmail.com>
2016-10-26 00:08:58 -0400

    Created Pages controller. Added home, index, profile and explore pages

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,19 @@
+# Contains the code for all the pages inside /pages
+class PagesController < ApplicationController
+  
+  # backend code  for pages/index
+  def index
+  end
+
+  # backend code for pages/home
+  def home
+  end
+
+  # backend code for pages/profile
+  def profile
+  end
+
+  # backend code for pages/explore
+  def explore
+  end
+end
