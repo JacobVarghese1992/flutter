82354ab1af49de43f2115c9a4c86d52c9d0e7dbe
Jacob Varghese <jacobv1992@gmail.com>
2016-10-27 10:35:38 -0400

    Routed to access profiles

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -11,6 +11,14 @@ class PagesController < ApplicationController
 
   # backend code for pages/profile
   def profile
+
+    # grab the usernameser
+
+    if(User.find_by_username(params[:id]))
+      @username = params[:id]
+    else
+      redirect_to root_path, :notice=> "User not found!"
+    end  
   end
 
   # backend code for pages/explore
